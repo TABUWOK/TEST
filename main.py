f02e04357950297ab4978141aa217229a3a4d235
TABUWOK <tabuencaeric@gmail.com>
2023-12-04 17:44:43 +0100

    f

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ st.text_input('Nom:')
 st.text_input('Cognoms:')
 
 st.selectbox('Edat:', ['5-7 (Prebenjamí)','8-9 (Benjamí)','10-11 (Aleví)','12-13 (Infantil)','14-15(Cadet)','16-18 (Juvenil)'])
+st.date_input('Date input')
 
 
 st.checkbox('Aceptes sortir en les imatges del club')
