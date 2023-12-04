fa294531ab75487f0849e36800ee811d91f0b821
TABUWOK <tabuencaeric@gmail.com>
2023-12-04 18:01:23 +0100

    a

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import streamlit as st
 from backend import *
 
 st.write("# FORMULARI INSCRIPCIÓ LES CORTS FC")
+st.image('https://www.google.com/url?sa=i&url=https%3A%2F%2Fcelescortsbarcelona.com%2F&psig=AOvVaw1ueEw9wYWHQ4X-gFRZsVN6&ust=1701795643076000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCKCrmPeg9oIDFQAAAAAdAAAAABAE')
 st.text_input('Nom:')
 st.text_input('Cognoms:')
 dni=st.text_input(label='DNI', max_chars=9)
