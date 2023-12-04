2be2becf5384aee604da2d5814f80b144e357215
TABUWOK <tabuencaeric@gmail.com>
2023-12-04 17:41:55 +0100

    a

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ st.write("# FORMULARI INSCRIPCIÓ LES CORTS FC")
 st.text_input('Nom:')
 st.text_input('Cognoms:')
 
-st.selectbox('Edat:', ['5-7','8-9','10-11','12-13','14-15','16-18'])
+st.selectbox('Edat:', ['5-7 (Prebenjamí)','8-9 (Benjamí)','10-11 (Aleví)','12-13 (Infantil)','14-15(Cadet)','16-18 (Juvenil)'])
 
 
 st.checkbox('Aceptes sortir en les imatges del club')
