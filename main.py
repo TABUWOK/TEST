0cae3a464c398d71fba63e6ed6c1aafb638c7b96
TABUWOK <tabuencaeric@gmail.com>
2023-12-04 17:35:13 +0100

    d

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ import streamlit as st
 from backend import *
 
 st.write("# FORMULARI INSCRIPCIÓ LES CORTS FC")
-st.write("Nom:") 
-st.text_input('Enter some text')
+st.text_input('Nom:')
+st.text_input('Cognoms:')
 
+st.selectbox('Select', [1,2,3])
 
 
 st.checkbox('Aceptes sortir en les imatges del club')
