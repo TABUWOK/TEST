e5728a7ee514ce91049e9454927252fa1ea9e412
TABUWOK <tabuencaeric@gmail.com>
2024-01-08 18:59:09 +0100

    s

--- pages/Fitxer1.py
+++ pages/Fitxer1.py
@@ -14,19 +14,6 @@ st.write("This is the main content area.")
 st.components.v1.html(
     """
     <script>
-    // Modify the decoration on top to reuse as a banner
-
-    // Locate elements
-    var decoration = window.parent.document.querySelectorAll('[data-testid="stDecoration"]')[0];
-    var sidebar = window.parent.document.querySelectorAll('[data-testid="stSidebar"]')[0];
-
-    // Observe sidebar size
-    function outputsize() {
-        decoration.style.left = `${sidebar.offsetWidth}px`;
-    }
-
-    new ResizeObserver(outputsize).observe(sidebar);
-
     // Adjust sizes
     outputsize();
     decoration.style.height = "3.0rem";
