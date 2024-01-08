9f9aec868f29c5ec46af54ca12e9c222dea7dce2
TABUWOK <tabuencaeric@gmail.com>
2024-01-08 18:51:03 +0100

    x

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import streamlit as st
 import Formulari
 
 st.markdown("<h1 style='text-align: center; color: #ab2a3e;'>COLLBLANC SANTS</h1>", unsafe_allow_html=True)
-st.set_page_config(layout="wide")
+
 custom_html = """
 <div class="banner">
     <img src="https://img.freepik.com/premium-photo/wide-banner-with-many-random-square-hexagons-charcoal-dark-black-color_105589-1820.jpg" alt="Banner Image">
@@ -19,8 +19,6 @@ custom_html = """
     }
 </style>
 """
-st.components.v1.html(custom_html)
-
 
 
 
