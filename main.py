c5f00a79cb1467d3987feff2b4201dbd403e4592
TABUWOK <tabuencaeric@gmail.com>
2024-01-08 18:43:20 +0100

    Z

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import streamlit as st
 import Formulari
 
 st.markdown("<h1 style='text-align: center; color: #ab2a3e;'>COLLBLANC SANTS</h1>", unsafe_allow_html=True)
-st.image('http://pbcollblanc.com/wp-content/uploads/2019/01/foto-balon-cabecera-3-1-e1548841828911.jpg')
+st.image('')
 
 
 
