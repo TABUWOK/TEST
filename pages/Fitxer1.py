95fbae131f301da54b53849f96dbc2421b0f3efe
TABUWOK <tabuencaeric@gmail.com>
2024-01-08 18:57:41 +0100

    a

--- pages/Fitxer1.py
+++ pages/Fitxer1.py
@@ -1,31 +1,39 @@
 import streamlit as st
-st.set_page_config(layout="wide")
-# Custom HTML/CSS for the banner
-custom_html = """
-<div class="banner">
-    <img src="https://img.freepik.com/premium-photo/wide-banner-with-many-random-square-hexagons-charcoal-dark-black-color_105589-1820.jpg" alt="Banner Image">
-</div>
-<style>
-    .banner {
-        width: 160%;
-        height: 200px;
-        overflow: hidden;
-    }
-    .banner img {
-        width: 100%;
-        object-fit: cover;
-    }
-</style>
-"""
-# Display the custom HTML
-st.components.v1.html(custom_html)
 
 # Sidebar content
 st.sidebar.header("Sidebar Title")
 st.sidebar.subheader("Subheading")
-st.sidebar.text("Sidebar content goes here.")
+st.sidebar.write("Sidebar content goes here.")
 
 # Main content
 st.title("Main Content")
 st.write("Welcome to my Streamlit app!")
 st.write("This is the main content area.")
+
+# JS code to modify te decoration on top
+st.components.v1.html(
+    """
+    <script>
+    // Modify the decoration on top to reuse as a banner
+
+    // Locate elements
+    var decoration = window.parent.document.querySelectorAll('[data-testid="stDecoration"]')[0];
+    var sidebar = window.parent.document.querySelectorAll('[data-testid="stSidebar"]')[0];
+
+    // Observe sidebar size
+    function outputsize() {
+        decoration.style.left = `${sidebar.offsetWidth}px`;
+    }
+
+    new ResizeObserver(outputsize).observe(sidebar);
+
+    // Adjust sizes
+    outputsize();
+    decoration.style.height = "3.0rem";
+    decoration.style.right = "45px";
+
+    // Adjust image decorations
+    decoration.style.backgroundImage = "url(https://img.pokemondb.net/sprites/emerald/normal/pikachu.png)";
+    decoration.style.backgroundSize = "contain";
+    </script>        
+    """, width=0, height=0)
