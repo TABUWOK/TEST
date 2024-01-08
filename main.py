359d50862ec04cac2ccec3c84404c7374a3e9b0b
TABUWOK <tabuencaeric@gmail.com>
2024-01-08 19:19:04 +0100

    s

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import streamlit as st
 import Formulari
 
 st.markdown("<h1 style='text-align: center; color: #ab2a3e;'>COLLBLANC SANTS</h1>", unsafe_allow_html=True)
-st.image('Imatges/Foto_jugadors.jpg', width=100)
+st.image('Imatges/Foto_jugadors.jpg', width=300)
 
 
 st.button('a', on_click=Formulari.form())
