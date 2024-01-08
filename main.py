c819dc81ffff33fbba5be364d961831a8e43a66c
TABUWOK <tabuencaeric@gmail.com>
2024-01-08 19:38:00 +0100

    s

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ st.markdown("<h1 style='text-align: center; color: #ab2a3e;'>COLLBLANC SANTS</h1
 e,d = st.columns(2)
 
 e.image('Imatges/Foto_jugadors.jpg', width=325)
-d.image('Imatges/Foto_jugadors.jpg', width=325)
 e.label("Titols")
+d.image('Imatges/Foto_jugadors.jpg', width=325)
 d.label("Equips")
 
 
