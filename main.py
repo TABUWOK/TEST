8cdb0cf854a300c6d8ed45fe85ecf7c38129669c
TABUWOK <tabuencaeric@gmail.com>
2024-01-08 19:36:39 +0100

    z

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ import Formulari
 st.markdown("<h1 style='text-align: center; color: #ab2a3e;'>COLLBLANC SANTS</h1>", unsafe_allow_html=True)
 e,d = st.columns(2)
 
-e.image('Imatges/Foto_jugadors.jpg', width=325, caption = "$Titols$")
-d.image('Imatges/Foto_jugadors.jpg', width=325, caption = ":blue[Equips] ")
-
-
+e.image('Imatges/Foto_jugadors.jpg', width=325)
+d.image('Imatges/Foto_jugadors.jpg', width=325)
+e.label("Titols")
+d.label("Equips")
 
 
 st.button('a', on_click=Formulari.form())
