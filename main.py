0c07f35428f72c9541bf5aa87434f5c0cee2ab1a
TABUWOK <tabuencaeric@gmail.com>
2024-01-08 19:50:58 +0100

    s

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import streamlit as st
-#import Formulari
+import Formulari
 
 st.markdown("<h1 style='text-align: center; color: #ab2a3e;'>COLLBLANC SANTS</h1>", unsafe_allow_html=True)
 e,d = st.columns(2)
@@ -12,5 +12,4 @@ with d:
 
 
 if st.button("a"):
-    nav_pages("Formulari")
-#st.button('a', on_click=Formulari.form())
+    Formulari.form()
