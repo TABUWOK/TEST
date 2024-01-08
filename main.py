3d41ba71d55a0a18d2b4fbe86c57fbce4faf5c73
TABUWOK <tabuencaeric@gmail.com>
2024-01-08 18:33:43 +0100

    a

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import Formulari
 
-st.markdown(style=text-align: center; color: red; "# COLLBLANC SANTS")
+st.write(style=text-align: center; color: red; "# COLLBLANC SANTS")
 
 
 
