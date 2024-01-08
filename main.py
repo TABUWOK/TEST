be62507aa9e68757d43b95754ea2c1e5e0bc0820
TABUWOK <tabuencaeric@gmail.com>
2024-01-08 19:39:42 +0100

    s

--- main.py
+++ main.py
@@ -3,11 +3,12 @@ import Formulari
 
 st.markdown("<h1 style='text-align: center; color: #ab2a3e;'>COLLBLANC SANTS</h1>", unsafe_allow_html=True)
 e,d = st.columns(2)
-
-e.image('Imatges/Foto_jugadors.jpg', width=325)
-e.label("Titols")
-d.image('Imatges/Foto_jugadors.jpg', width=325)
-d.label("Equips")
+with e:
+    st.image('Imatges/Foto_jugadors.jpg', width=325)
+    st.label("Titols")
+with d:
+    d.image('Imatges/Foto_jugadors.jpg', width=325)
+    d.label("Equips")
 
 
 st.button('a', on_click=Formulari.form())
