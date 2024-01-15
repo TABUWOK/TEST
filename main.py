282924f09e941958eabf5f1b97a7b1efa2eda733
TABUWOK <tabuencaeric@gmail.com>
2024-01-15 18:42:58 +0100

    a

--- main.py
+++ main.py
@@ -14,8 +14,16 @@ elif st.session_state["Pagina"] == "main":
     with e:
         st.image('https://futbolcatalunya.com/wp-content/uploads/2022/01/equipmercantil.jpg', width=325)
         st.markdown("<p style='text-align: center; color: black;'>La nostra historia</p>", unsafe_allow_html=True)
+        
+        st.image('https://pbs.twimg.com/media/D5L-d-OWsAgXAR1.jpg', width=325)
+        st.markdown("<p style='text-align: center; color: black;'>Títols</p>", unsafe_allow_html=True)
+
+
     with d:
-        d.image('Imatges/Foto_jugadors.jpg', width=375)
+        d.image('Imatges/Foto_jugadors.jpg', width=355)
         st.markdown("<p style='text-align: center; color: black;'>Equips</p>", unsafe_allow_html=True)
+        
+        d.image('https://www.mundodeportivo.com/files/image_948_465/uploads/2022/11/21/637b5a299a357.jpeg', width=325)
+        st.markdown("<p style='text-align: center; color: black;'>Calendari partits</p>", unsafe_allow_html=True)
     
     
