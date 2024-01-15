0001e00dbdad8ca8f9b5e49eb291ea09696afee3
TABUWOK <tabuencaeric@gmail.com>
2024-01-15 18:33:48 +0100

    s

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ elif st.session_state["Pagina"] == "main":
     st.markdown("<h1 style='text-align: center; color: #ab2a3e;'>COLLBLANC SANTS</h1>", unsafe_allow_html=True)
     e,d = st.columns(2)
     with e:
-        st.image('Imatges/Foto_jugadors.jpg', width=325)
+        st.image('https://www.google.com/url?sa=i&url=https%3A%2F%2Ffutbolcatalunya.com%2Fmurtro-adeu-a-un-gran-del-futbol-catala%2F&psig=AOvVaw39KDkFBAt9YN5cKXugr5CN&ust=1705426356183000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCPCQ7bv234MDFQAAAAAdAAAAABAQ', width=325)
         st.markdown("<p style='text-align: center; color: black;'>La nostra historia</p>", unsafe_allow_html=True)
     with d:
         d.image('Imatges/Foto_jugadors.jpg', width=325)
