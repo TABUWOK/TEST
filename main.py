de0522f1815380ae363d6cd6e967b8dbbe9fd5f1
TABUWOK <tabuencaeric@gmail.com>
2024-01-15 18:35:05 +0100

    s

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ elif st.session_state["Pagina"] == "main":
         st.image('https://futbolcatalunya.com/wp-content/uploads/2022/01/equipmercantil.jpg', width=325)
         st.markdown("<p style='text-align: center; color: black;'>La nostra historia</p>", unsafe_allow_html=True)
     with d:
-        d.image('Imatges/Foto_jugadors.jpg', width=325)
+        d.image('Imatges/Foto_jugadors.jpg', width=350)
         st.markdown("<p style='text-align: center; color: black;'>Equips</p>", unsafe_allow_html=True)
     
     
