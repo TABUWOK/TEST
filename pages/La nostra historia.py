c2766bd2695570ff68066d418d622d598776d5a2
TABUWOK <tabuencaeric@gmail.com>
2024-01-15 19:18:19 +0100

    s

---
+++ pages/La nostra historia.py
@@ -0,0 +1,19 @@
+import streamlit as st
+
+st.markdown("<h1 style='text-align: center; color: #ab2a3e;'>COLLBLANC SANTS</h1>", unsafe_allow_html=True)
+st.write("Els seus orígens es remonten a l’any 1956, tenint com a objectiu, la pràctica de l’esport en un moment social complex de la vida espanyola i catalana, amb dificultats per reunir-se. Aquest tipus d’associacions esportives eren l’excusa apropiada per a reunir-se legalment y debatir sobre la situació política.")
+st.write("La A.E.B Collblanc que anteriorment es deia PEÑA KUBALA, va canviar de nom perquè els membres de la Junta Directiva eren comerciants d’aquest mateix barri.")
+ 
+st.write("Els nostres equips juguen en les següents instal·lacions:")
+st.write("– Camps de Mini FC Barcelona")
+st.write("– Camp Aristides Maillol, al costat del Camp Nou")
+ 
+st.write("Tots els equips estan federats i participen en les competicions organitzades per la Federació Catalana de Futbol, a més d’ésser un equip que milita en el Consell Escolar.")
+ 
+st.write("Actualment mantenim 19 equips en competició de Futbol base des de pre-benjamins fins a juvenils, així com un equip Amateur que miliatará a Quarta Catalana. En total s’aconsegueix que unes 300 famílies mantiguin un vincle d’unió amb la nostra Entitat Esportiva.")
+ 
+st.write("Al cap i a la fi, del que es tracta es de tenir una identitat i cultura pròpies que ens caracteritzi i ens diferenciï positivament de la resta d’entitats. En la nostra Entitat, el respecte cap al contrari, els companys, els àrbitres, etc han d’ésser una premisa prioritària.")
+ 
+st.write("Aportar als nostres jugadors una formació completa a través de l’esport. Competir i formar, per això tots els jugadors tenen un lloc al nostre club, amb una oferta de categories àmplia que inclou totes les edats.")
+ 
+st.write("El que pretenem es formar com a esportista i persona a tot jugador que comparteixi el nostre objectiu com a Entitat Esportiva.")
