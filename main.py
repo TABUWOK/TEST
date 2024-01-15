3ddeb6176ee8350d4651df0ef3218aa1eb2df618
TABUWOK <tabuencaeric@gmail.com>
2024-01-15 18:11:01 +0100

    s

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ st.session_state["Pagina"] = "main"
 
 if st.button("a"):
     st.session_state["Pagina"] = "Formulari"
-    st.write(st.session_state["Pagina"])
+    Formulari.form()
 else:
     st.markdown("<h1 style='text-align: center; color: #ab2a3e;'>COLLBLANC SANTS</h1>", unsafe_allow_html=True)
     e,d = st.columns(2)
@@ -15,10 +15,5 @@ else:
     with d:
         d.image('Imatges/Foto_jugadors.jpg', width=325)
         st.markdown("<p style='text-align: center; color: black;'>Equips</p>", unsafe_allow_html=True)
-
-
-
-
-if st.session_state["Pagina"] == "Formulari":
-    Formulari.form()
+    
     
