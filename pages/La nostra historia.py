fea9945ee10b6802b108788ef1b7eaa6a25b4abe
TABUWOK <tabuencaeric@gmail.com>
2024-01-22 18:20:03 +0100

    s

--- pages/La nostra historia.py
+++ pages/La nostra historia.py
@@ -1,13 +1,16 @@
 import streamlit as st
 
-st.markdown("<h1 style='text-align: center; color: #ab2a3e;'>COLLBLANC SANTS</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center; color: #ab2a3e;'>COLLBLANC SANTS: LA NOSTRA HISTÒRIA</h1>", unsafe_allow_html=True)
 st.write("Els seus orígens es remonten a l’any 1956, tenint com a objectiu, la pràctica de l’esport en un moment social complex de la vida espanyola i catalana, amb dificultats per reunir-se. Aquest tipus d’associacions esportives eren l’excusa apropiada per a reunir-se legalment y debatir sobre la situació política.")
 st.write("La A.E.B Collblanc que anteriorment es deia PEÑA KUBALA, va canviar de nom perquè els membres de la Junta Directiva eren comerciants d’aquest mateix barri.")
- 
-st.write("Els nostres equips juguen en les següents instal·lacions:")
-st.write("– Camps de Mini FC Barcelona")
-st.write("– Camp Aristides Maillol, al costat del Camp Nou")
- 
+e,d = st.columns(2)
+with e:
+    st.write("Els nostres equips juguen en les següents instal·lacions:")
+    st.write("– Camps de Mini FC Barcelona")
+    st.write("– Camp Aristides Maillol, al costat del Camp Nou")
+with d:
+    st.image("https://files.fcf.cat/campos/2015-11-15-15:10:20340.jpeg", width=325)
+
 st.write("Tots els equips estan federats i participen en les competicions organitzades per la Federació Catalana de Futbol, a més d’ésser un equip que milita en el Consell Escolar.")
  
 st.write("Actualment mantenim 19 equips en competició de Futbol base des de pre-benjamins fins a juvenils, així com un equip Amateur que miliatará a Quarta Catalana. En total s’aconsegueix que unes 300 famílies mantiguin un vincle d’unió amb la nostra Entitat Esportiva.")
