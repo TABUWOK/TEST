9456c9bb17cc3f753798a64760c3b2cd114e0a3c
TABUWOK <tabuencaeric@gmail.com>
2024-01-22 18:41:41 +0100

    S

--- pages/Equips.py
+++ pages/Equips.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.markdown("<h1 style='text-align: center; color: #ab2a3e;'>COLLBLANC SANTS: EQUIPS</h1>", unsafe_allow_html=True)
 st.write("1ra Divisió:")
-[
-    0: "S"
-    1: "D"
-]
+lst = ['a', 'b', 'c']
+
+for i in lst:
+    st.markdown("- " + I)
