da02fa68fbd6d64298f63e31cb40a34629c94a91
TABUWOK <tabuencaeric@gmail.com>
2024-01-22 18:52:16 +0100

    A

--- pages/Equips.py
+++ pages/Equips.py
@@ -2,8 +2,29 @@ import streamlit as st
 
 st.markdown("<h1 style='text-align: center; color: #ab2a3e;'>COLLBLANC SANTS: EQUIPS</h1>", unsafe_allow_html=True)
 st.write("1ra Divisió:")
-lst = ['a', 'b', 'c']
+lst = ['a']
 
 for i in lst:
-    st.markdown("- " + "MESSI")
-    st.markdown("- " + "CR7")
+    st.markdown("- " + "Pre-benjami A")
+    st.markdown("- " + "Pre-benjami B")
+    st.markdown("- " + "Benjami A")
+    st.markdown("- " + "Alevi A")
+    st.markdown("- " + "Infantil A")
+    st.markdown("- " + "Infantil B")
+    st.markdown("- " + "Cadet A")
+    st.markdown("- " + "Juvenil A")
+
+st.write("2na Divisió:")
+lst = ['a']
+
+for i in lst:
+    st.markdown("- " + "Pre-benjami C")
+    st.markdown("- " + "Benjami B")
+    st.markdown("- " + "Benjami C")
+    st.markdown("- " + "Alevi B")
+    st.markdown("- " + "Infantil C")
+    st.markdown("- " + "Cadet B")
+    st.markdown("- " + "Cadet C")
+    st.markdown("- " + "Juvenil B")
+    st.markdown("- " + "Juvenil C")
+    
